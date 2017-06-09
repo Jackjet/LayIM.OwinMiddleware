92a430d329be416473b2d515f98737cab2e74358
Fanyuepan <fanpan26@126.com>
2017-06-09 17:30:27 +0800

    add base extensions

---
+++ LayIM.NetClient/Extensions.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LayIM.NetClient
+{
+    internal static class Extensions
+    {
+        public static int ToInt32(this string value)
+        {
+            int result = 0;
+            int.TryParse(value, out result);
+            return result;
+        }
+
+        public static long ToInt64(this string value)
+        {
+            long result = 0;
+            long.TryParse(value, out result);
+            return result;
+        }
+
+        public static long ToTimestamp(this DateTime time)
+        {
+            DateTime startTime = TimeZone.CurrentTimeZone.ToLocalTime(new DateTime(1970, 1, 1));
+            return (long)(time - startTime).TotalSeconds;
+        }
+    }
+}
